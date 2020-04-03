c78839a26ba06e239ac2c9f6486f225ef9fbd0c5
makender103 <makender103@gmail.com>
2020-04-03 07:13:12 -0300

    Complete Job Finder

---
+++ routes/jobs.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+const Job = require("../models/Job");
+
+//rota de teste
+router.get("/test", (req,res) => {
+    res.send(" Done !");
+});
+
+//detalhe da vaga view/1,view/2
+router.get("/view/:id", (req , res) => Job.findOne ({
+    where : {id: req.params.id}
+}).then(job =>{
+    res.render("view", {
+       job 
+    });
+}) .catch(err => console.log(err)));
+
+//rota da Form de envio
+router.get("/add", ( req, res) => {
+    res.render("add");
+});
+//add Job via post
+router.post("/add", (req, res) => {
+   let {title, salary, company, description, email, new_job} = req.body;
+    //inserir dados no sistema
+    Job.create ({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err));
+});
+
+module.exports = router
